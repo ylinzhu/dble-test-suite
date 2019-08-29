ec90244a7338711357240b7d085d61000e509f86
zhaohongjie <zhaohongjie@actionsky.com>
2019-08-29 10:27:42 +0800

    fix query syntax error

--- behave_dble/sqls/special_nosharding/select/join_union_subquery_mixed.sql
+++ behave_dble/sqls/special_nosharding/select/join_union_subquery_mixed.sql
@@ -8,6 +8,6 @@ insert into noshard_t1 values(1,1,'test中id为1',1),(2,2,'test_2',2),(3,3,'test
 insert into noshard_t2 values(1,1,'order中id为1',1),(2,2,'test_2',2),(3,3,'order中id为3',3),(4,4,'$order$4',4),(5,5,'order...5',1);
 insert into noshard_t3 values(1,1,'manager中id为1',1),(2,2,'test_2',2),(3,3,'manager中id为3',3),(4,4,'$manager$4',4),(5,5,'manager...5',6);
 
-select * from (select id, t_id, name,pad from noshard_t1 a join noshard_t3 b on a.id = b.id union select * from  noshard_t2) as c order by id,name;
+select * from (select a.id, t_id, a.name, a.pad from noshard_t1 a join noshard_t3 b on a.id = b.id union select * from  noshard_t2) as c order by id,name;
 select id, t_id, name,pad from (select a.id,t_id, b.name,b.pad from noshard_t1 a join noshard_t3 b on a.id = b.id) as c union select * from noshard_t2 order by id, name;
-select * from (select * from ( select a.id, t_id, a.name,a.pad from noshard_t1 a join noshard_t2 b on a.id=b.id union (select * from  noshard_t2 where id in (select id from noshard_t3))) as d UNION select * from noshard_t3) as c order by c.id,c.name;;
+select * from (select * from ( select a.id, t_id, a.name,a.pad from noshard_t1 a join noshard_t2 b on a.id=b.id union (select * from  noshard_t2 where id in (select id from noshard_t3))) as d UNION select * from noshard_t3) as c order by c.id,c.name;
